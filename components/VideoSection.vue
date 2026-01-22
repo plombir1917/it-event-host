60cac7cd99d046b0bdedf56e5913ead19e7f3455
KopichZS <KopichZS@admlr.lipetsk.ru>
2026-01-22 12:24:46 +0300

    fix(VideoSection): remove `lazy` loading from iframe

--- components/VideoSection.vue
+++ components/VideoSection.vue
@@ -71,7 +71,6 @@
               allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
               frameborder="0"
               allowfullscreen
-
               @load="handleVideoLoad"
             ></iframe>
             <template #fallback>
