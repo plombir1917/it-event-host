545ad0471a2a6475e19545baf44d682ed9524bd5
KopichZS <KopichZS@admlr.lipetsk.ru>
2026-01-23 11:43:08 +0300

    refactor(nuxt.config): title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: 'Ведущий-Программист | IT Event Host',
+      title: 'Ведущий-Программист | Захар Копич',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
